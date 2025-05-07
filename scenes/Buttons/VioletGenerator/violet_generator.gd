513c973905bbe36d49591240d7f8fc32614feb2d
MaleVasle <74022389+MaleVale@users.noreply.github.com>
2025-05-06 23:02:51 -0300

    agregar bloques a grupos
    c:

--- scenes/Buttons/VioletGenerator/violet_generator.gd
+++ scenes/Buttons/VioletGenerator/violet_generator.gd
@@ -1,5 +1,6 @@
 extends TextureButton
 
+@onready var BlockViolet = preload("res://scenes/Buttons/VioletGenerator/VioletGenerator.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,6 +11,9 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func generate_block():
+	var new_block = BlockViolet.instantiate()
+	get_tree().current_scene.add_child(new_block)
 
 func _on_button_down() -> void:
-	pass # Replace with function body.
+	generate_block()
