34af11a10bbf188cae5d7e0b6d98e79ad26401fc
MaleVasle <74022389+MaleVale@users.noreply.github.com>
2025-05-06 22:59:32 -0300

    generar bloques
    :((

--- scenes/Buttons/RedGenerator/red_generator.gd
+++ scenes/Buttons/RedGenerator/red_generator.gd
@@ -1,4 +1,5 @@
 extends TextureButton
+@onready var BlockRed = preload("res://scenes/Blocks/Red/BlockRed.tscn")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,6 +11,9 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func generate_block():
+	var new_block = BlockRed.instantiate()
+	get_tree().current_scene.add_child(new_block)
 
 func _on_button_down() -> void:
-	pass # Replace with function body.
+	generate_block()
