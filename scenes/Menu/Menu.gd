f7eb5cf72dee6edf7df4e2ad8aab6a1cd6fda377
MaleVasle <74022389+MaleVale@users.noreply.github.com>
2025-05-06 23:09:45 -0300

    conectar señal reset

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -9,3 +9,8 @@ func _ready():
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
 	pass
+
+
+func _on_reset_button_down() -> void:
+	for block in get_tree().get_nodes_in_group("blocks"):
+		block.delete()
